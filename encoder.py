4bb99aaedbac929db46697c4d9dd20255b62db90
Alireza <alirezanadafian0@gmail.com>
2018-08-23 16:31:39 +0430

    return_raw_data

--- encoder.py
+++ encoder.py
@@ -3,12 +3,13 @@ from keras.layers import Input, LSTM, RepeatVector
 from keras.models import Model
 from load_data import load_data
 from keras.callbacks import EarlyStopping
+from keras import optimizers
 
 
 def fix_size_of_data(ts_data):
 	reconstructed_ts_data = np.zeros((len(ts_data) , 120 , 13))
 	for i in range(len(ts_data)):
-		reconstructed_ts_data[i] = ts_data[i][:120 , :]
+		reconstructed_ts_data[i] = ts_data[i][:120 ]
 	return reconstructed_ts_data
 
 def normalize(ts_data):
@@ -21,18 +22,16 @@ def normalize(ts_data):
 def get_encoder(ts_data):
 	timesteps = len(ts_data[0])
 	input_dim = len(ts_data[0][0])
-	latent_dim = 15
+	latent_dim = 10
 	inputs = Input(shape=(timesteps, input_dim))
-	encoded = LSTM(latent_dim , dropout=0.75)(inputs)
-
+	encoded = LSTM(latent_dim , dropout=0.5)(inputs)
 	decoded = RepeatVector(timesteps)(encoded)
 	decoded = LSTM(input_dim, return_sequences=True)(decoded)
 
 	sequence_autoencoder = Model(inputs, decoded)
-	sequence_autoencoder.compile(optimizer='adam', loss='mse')
-	sequence_autoencoder.fit(ts_data, ts_data, epochs=1000,
-							batch_size=len(ts_data), shuffle=True ,
-							validation_split = 1/10, callbacks = [EarlyStopping(monitor='val_loss',patience = 5)] )
+	sequence_autoencoder.compile(optimizer=optimizers.Adam(decay = 1e-6), loss='mse')
+	sequence_autoencoder.fit(ts_data, ts_data, epochs=500,
+							batch_size=len(ts_data), shuffle=True)
 	encoder = Model(inputs, encoded)
 	return encoder
 
@@ -40,6 +39,7 @@ def get_encoded_data():
 	ts_data , type_data = load_data()
 	ts_data = fix_size_of_data(ts_data)
 	ts_data = normalize(ts_data)
+	return ts_data
 	encoder = get_encoder(ts_data)
 	return encoder.predict(ts_data)
 
